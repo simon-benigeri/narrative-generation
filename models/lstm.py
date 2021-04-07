e257ff437bf09553592fcd3f3b2e018c975b1d80
simon-benigeri <simon.benigeri@protonmail.ch>
2021-04-07 10:31:31 +0200

    some code from elsewhere that has very little to do with this project

---
+++ models/lstm.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+from typing import List, Tuple
+
+class LSTM_Model(nn.Module):
+    def __init__(self, device:str, vocab_size:int, max_grad:float, embed_dims:int, num_layers:int,
+                 dropout_prob:float, init_range:float, bias:bool):
+        """
+        :param device: cpu, gpu
+        :param vocab_size: vocabulary size
+        :param max_grad: gradient clipped at this value
+        :param embed_dims: output dimensions of embedding layer
+        :param num_layers: number of lstm layers
+        :param dropout_prob: dropout probability
+        :param init_range: weights are initialized by drawing samples
+                        from uniform distribution over interval [init_range, init_range]
+        :param bias: if true, we use bias weights in nn layers
+        """
+        super().__init__()
+        self.max_grad = max_grad
+        self.init_param = init_range
+
+        # model architecture
+        self.dropout = nn.Dropout(p=dropout_prob)
+        self.embed = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_dims)
+        lstms = [nn.LSTM(input_size=embed_dims, hidden_size=embed_dims, bias=bias, batch_first=True)
+                 for _ in range(num_layers)
+                 ]
+        self.lstms = nn.ModuleList(modules=lstms)
+        self.fc = nn.Linear(in_features=embed_dims, out_features=vocab_size)
+
+        # initialize model weights
+        for param in self.parameters():
+            nn.init.uniform_(param, -self.init_param, self.init_param)
+
+        self.device = torch.device("cuda" if device == "gpu" and torch.cuda.is_available() else "cpu")
+
+
+    def init_state(self, batch_size:int) -> List[Tuple[Tensor, Tensor]]:
+        """
+        Initialize weights for hidden states in lstm layers
+        :param batch_size: batch size
+        :return: initialized weights
+        """
+        dev = next(self.parameters()).device
+        states = [
+            (torch.zeros(1, batch_size, layer.hidden_size, device=dev),
+             torch.zeros(1, batch_size, layer.hidden_size, device=dev))
+                  for layer in self.lstms
+        ]
+        return states
+
+    def detach_states(self, states:List[Tuple[Tensor, Tensor]]) -> List[Tuple[Tensor, Tensor]]:
+        """
+        Detach output from computational graph
+        :param states: lstm weights
+        :return: detached view of data
+        """
+        return [(h.detach(), c.detach()) for (h, c) in states]
+
+    def forward(self, x:Tensor, states:List[Tuple[Tensor, Tensor]]) -> Tuple[Tensor, List[Tuple[Tensor, Tensor]]]:
+        """
+        Forward pass through lstm model
+        :param x: data with shape (batch_size, time_steps)
+        :param states: lstm weights
+        :return: (unnormalized token probabilities, lstm weights)
+        """
+        x = self.embed(x)
+        x = self.dropout(x)
+        for i, lstm in enumerate(self.lstms):
+            x, states[i] = lstm(x, states[i])
+            x = self.dropout(x)
+        scores = self.fc(x)
+        return scores, states
---
+++ pipelines/datasets.py
@@ -0,0 +1,146 @@
+import os
+import time
+from io import open
+from typing import List, Dict, Tuple
+import numpy as np
+import nltk
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset, DataLoader
+
+class Sequence_Dataset(Dataset):
+    def __init__(self, x:torch.LongTensor, y:torch.LongTensor):
+        self.x = x
+        self.y = y
+        self.len = x.shape[0]
+
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+
+    def __len__(self):
+        return self.len
+
+def _load_text_data(path: str) -> str:
+    """
+    read text file
+    :param path:
+    :return:
+    """
+    with open(path, 'r') as f:
+        text = f.read()
+    return text
+
+
+def _tokenize(text: str) -> List[str]:
+    """
+    Remove unwanted tokens like <s> and \n
+    :param text: tokens separated by ' '
+    :return: list of tokens
+    """
+    text = text.replace('<s>', '')
+    text = text.replace('\n', ' ')
+    text = ' '.join(text.split())
+    text = text.lower()
+    words = text.strip().split(' ')
+    return words
+
+def _init_corpora(path:str, topic:str, freq_threshold:int,) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Dict[str, int]]:
+    """
+    :param path:
+    :param topic:
+    :return:
+    """
+    # read the text
+    train = _load_text_data(os.path.join(path, f"{topic}.train.txt"))
+    valid = _load_text_data(os.path.join(path, f"{topic}.valid.txt"))
+    test = _load_text_data(os.path.join(path, f"{topic}.test.txt"))
+
+    if topic == 'wiki':
+        # remove wiki tags
+        train = ' '.join(train.replace('=', ' ').split())
+        valid = ' '.join(valid.replace('=', ' ').split())
+        test = ' '.join(test.replace('=', ' ').split())
+
+    # split into word/token
+    train = _tokenize(text=train)
+    valid = _tokenize(text=valid)
+    test = _tokenize(text=test)
+
+    # apply frequency threshold to training set
+    freq_dist = nltk.FreqDist(train)
+    train = ['<unk>' if freq_dist[t] < freq_threshold else t for t in train]
+
+    # create vocabulary: set of words in train and word to index mapping
+    vocab = sorted(set(train))
+    word2index = {word: index+1 for index, word in enumerate(vocab)}
+    word2index['<pad>'] = 0
+
+    # convert each word to a list of integers. if word is not in vocab, we use unk
+    train = [word2index[word] for word in train]
+    valid = [word2index[word] if word in word2index else word2index['<unk>'] for word in valid]
+    test = [word2index[word] if word in word2index else word2index['<unk>'] for word in test]
+
+    # return (n, ) arrays for train, valid, test, and the word2index dict
+    return np.array(train), np.array(valid), np.array(test), word2index
+
+def _generate_io_sequences(sequence: np.ndarray, time_steps: int) -> Tuple:
+    """
+    :param sequence: sequence of integer representation of words
+    :param time_steps: number of time steps in LSTM cell
+    :return: Tuple of torch tensors of shape (n, time_steps)
+    """
+    sequence = torch.LongTensor(sequence)
+
+    # from seq we generate 2 copies.
+    inputs, targets = sequence, sequence[1:]
+
+    # split seq into seq of of size time_steps
+    inputs = torch.split(tensor=inputs, split_size_or_sections=time_steps)
+    targets = torch.split(tensor=targets, split_size_or_sections=time_steps)
+
+    # recall: word2index['<pad>'] = 0
+    inputs_padded = pad_sequence(sequences=inputs, batch_first=True, padding_value=0)
+    targets_padded = pad_sequence(sequences=targets, batch_first=True, padding_value=0)
+
+    return (inputs_padded, targets_padded)
+
+
+def _build_dataloader(data:np.ndarray, time_steps:int, batch_size:int) -> DataLoader:
+    """
+    :param data: input list of integers
+    :param batch_size: hyper parameter, for mini-batch size
+    :param time_steps: hyper parameter for sequence length for bptt
+    :return: DataLoader for SGD
+    """
+    # given int list, generate input and output sequences of length = time_steps
+    inputs, targets = _generate_io_sequences(sequence=data, time_steps=time_steps)
+    
+    # cut off any data that will create incomplete batches
+    num_batches = len(inputs) // batch_size
+    inputs = inputs[:num_batches*batch_size]
+    targets = targets[:num_batches*batch_size]
+    
+    # create Dataset object and from it create data loader
+    dataset = Sequence_Dataset(x=inputs, y=targets)
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+
+def init_datasets(topic:str, freq_threshold:int, time_steps:int, batch_size:int, path:str) -> Dict:
+    """
+    :param path: path to data files: [topic].train.txt
+    :param topic: [topic].train.txt, where topic can be wikitext, or nyt_covid
+    :param freq_threshold: hyperparameter words in training set with freq < threshold are replaced by '<unk>'
+    :param time_steps: hyperparameter number of time steps and therefore seq_length for bptt
+    :param batch_size: hyperparameter batch size
+    :return: datasets dict
+    """
+    train, valid, test, word2index = _init_corpora(path=path, topic=topic, freq_threshold=freq_threshold)
+    train_loader = _build_dataloader(data=train, time_steps=time_steps, batch_size=batch_size)
+    valid_loader = _build_dataloader(data=valid, time_steps=time_steps, batch_size=batch_size)
+    test_loader = _build_dataloader(data=test, time_steps=time_steps, batch_size=batch_size)
+    datasets = {
+        'data_loaders': (train_loader, valid_loader, test_loader),
+        'word2index': word2index,
+        'vocab_size': len(word2index)
+    }
+    return datasets
