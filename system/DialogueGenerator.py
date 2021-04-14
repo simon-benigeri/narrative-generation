0d708e5e917e4fb3c38533be66263e66d4ff051d
simon-benigeri <simon.benigeri@protonmail.ch>
2021-04-14 08:18:26 -0500

    cleaned up all that useless code form before

--- system/DialogueGenerator.py
+++ system/DialogueGenerator.py
@@ -6,18 +6,22 @@ Component to generate dialogue between characters
 class DialogueGenerator:
     def __init__(self):
         # Load all transformers
-        self.happy_transformer = ...
-        ...
+        self.happy_transformer = "happy dialogue"
+        self.sad_transformer = "sad dialogue"
+        self.anger_transformer = "angry dialogue"
+        # ...
 
         # Dictionary for emotion and corresponding transformer
         self.transformers = {
             "HAPPY": self.happy_transformer,
-            ...
+            "SAD": self.sad_transformer,
+            "ANGER": self.anger_transformer# ,
+            # ...
         }
     
     # Generate next character utterances given direction and past utterences
     def generate_utterances(self, emotion:str, prev_direction:str, prev_utterance:str):
-        # Generate utterences
+        # Generate utterances
         # ...
 
-        return utterences
+        return utterances
--- system/Script.py
+++ system/Script.py
@@ -6,11 +6,13 @@ Component to hold the script
 """
 class Script:
     # Named tuple for a single line in the script
-    ScriptLine = namedtuple('ScriptLine', ['line_num', 'arc_stage', 'type', 'text'])
+    ScriptLine = namedtuple('ScriptLine', ['line_num', 'arc_stage', 'type', 'character','text'])
 
     def __init__(self, arc: List[str]):
         self.DIRECTION = 0
         self.UTTERANCE = 1
+        self.CHARACTER_X = 'Character_X'
+        self.CHARACTER_Y = 'Character_Y'
 
         # Set story arc
         self.arc = arc
@@ -19,6 +21,8 @@ class Script:
 
         # List of lines in the script
         self.script_lines = []
+
+        # TODO: initial directions?
     
     # Increment arc stage
     def arc_step(self):
@@ -31,18 +35,19 @@ class Script:
             self.complete = True
 
     # Append list of utterances to script
-    def append_dialogue(self, utterances: List[str]):
+    def append_dialogue(self, character: str, utterances: List[str]):
         for u in utterances:
             self.script_lines.append(
-                self.ScriptLine(len(self.script_lines), self.current_arc_stage, self.UTTERANCE, u))
+                self.ScriptLine(len(self.script_lines), self.current_arc_stage, self.UTTERANCE, character, u))
     
     # Append direction to script
-    def append_direction(self, direction: str):
-        self.script_lines.append(
-            self.ScriptLine(len(self.script_lines), self.current_arc_stage, self.DIRECTION, direction))
+    def append_direction(self, character: str, directions: List[str]):
+        for d in directions:
+            self.script_lines.append(
+                self.ScriptLine(len(self.script_lines), self.current_arc_stage, self.DIRECTION, character, d))
 
     # Get the last n utterances from the script
-    def get_prev_utterances(self, n: int):
+    def get_prev_utterances(self, n: int, character: str):
         # ...
         count, index = 0, 0
         prev_utterances = []
@@ -51,13 +56,13 @@ class Script:
                 break
             index -= 1
             line = self.script_lines[index]
-            if line.type == self.DIRECTION:
+            if line.type == self.UTTERANCE and line.character == character:
                 prev_utterances.insert(0, line)
                 count += 1
         return prev_utterances
     
     # Get the last n directions from the script
-    def get_prev_directions(self, n: int):
+    def get_prev_directions(self, n: int, character: str):
         # ...
         count, index = 0, 0
         prev_directions = []
@@ -66,13 +71,23 @@ class Script:
                 break
             index -= 1
             line = self.script_lines[index]
-            if line.type == self.DIRECTION:
+            if line.type == self.DIRECTION and line.character == character:
                 prev_directions.insert(0, line)
                 count += 1
         return prev_directions
 
         # Get the last n directions from the script
-    def get_prev_lines(self, n: int, type: int):
+    def get_prev_lines(self, n: int, type: int, character: str):
+        """
+        returns previous utterances or screen directions for a character
+        Args:
+            n: number of utterances
+            type: utterance or direction
+            character: X or Y
+
+        Returns: prev n directions or utterances for a character
+
+        """
         # ...
         count, index = 0, 0
         prev = []
@@ -81,7 +96,7 @@ class Script:
                 break
             index -= 1
             line = self.script_lines[index]
-            if line.type == type:
+            if line.type == type and line.character == character:
                 prev.insert(0, line)
                 count += 1
         return prev
@@ -90,20 +105,44 @@ class Script:
     def __str__(self):
         script_str = ""
         for l in self.script_lines:
-            script_str += l.text + "\n"
+            script_str += f"{l.character} : {l.text}" + "\n"
         return script_str
 
 if __name__ =='__main__':
     arc = ['sad', 'happy']
     s = Script(arc)
-    N = 5
+    N = 3
     utterances = [f"utterance {i}" for i in range(N)]
     directions = [f"direction {i}" for i in range(N)]
     for u, d in zip(utterances, directions):
-        s.append_dialogue([u])
-        s.append_direction(d)
+        s.append_dialogue(s.CHARACTER_X, [u])
+        s.append_dialogue(s.CHARACTER_Y, [u])
+        s.append_direction(s.CHARACTER_X, d)
+        s.append_direction(s.CHARACTER_Y, d)
 
-    # print(str(s))
-    assert len(s.get_prev_utterances(n=N+1)) == len(utterances)
-    assert len(s.get_prev_directions(n=0)) == 0
-    assert len(s.get_prev_directions(n=3)) == 3
+    print(str(s))
+
+    assert len(s.get_prev_utterances(n=0, character=s.CHARACTER_X)) == 0
+    assert len(s.get_prev_utterances(n=0, character=s.CHARACTER_Y)) == 0
+    assert len(s.get_prev_utterances(n=2, character=s.CHARACTER_X)) == 2
+    assert len(s.get_prev_utterances(n=2, character=s.CHARACTER_Y)) == 2
+    assert len(s.get_prev_utterances(n=N + 2, character=s.CHARACTER_X)) == N
+    assert len(s.get_prev_utterances(n=N + 2, character=s.CHARACTER_Y)) == N
+
+    assert len(s.get_prev_directions(n=0, character=s.CHARACTER_X)) == 0
+    assert len(s.get_prev_directions(n=0, character=s.CHARACTER_Y)) == 0
+    assert len(s.get_prev_directions(n=2, character=s.CHARACTER_X)) == 2
+    assert len(s.get_prev_directions(n=2, character=s.CHARACTER_Y)) == 2
+    assert len(s.get_prev_directions(n=N + 2, character=s.CHARACTER_X)) == N
+    assert len(s.get_prev_directions(n=N + 2, character=s.CHARACTER_Y)) == N
+
+    assert len(s.get_prev_lines(n=0, type=s.UTTERANCE, character=s.CHARACTER_X)) == 0
+    assert len(s.get_prev_lines(n=0, type=s.UTTERANCE, character=s.CHARACTER_Y)) == 0
+    assert len(s.get_prev_lines(n=2, type=s.UTTERANCE, character=s.CHARACTER_X)) == 2
+    assert len(s.get_prev_lines(n=2, type=s.UTTERANCE, character=s.CHARACTER_Y)) == 2
+    assert len(s.get_prev_lines(n=2, type=s.DIRECTION, character=s.CHARACTER_X)) == 2
+    assert len(s.get_prev_lines(n=2, type=s.DIRECTION, character=s.CHARACTER_Y)) == 2
+    assert len(s.get_prev_lines(n=N + 2, type=s.UTTERANCE, character=s.CHARACTER_X)) == N
+    assert len(s.get_prev_lines(n=N + 2, type=s.DIRECTION, character=s.CHARACTER_Y)) == N
+
+    # assert len(s.get_prev_lines(n=2, type=s.DIRECTION, character=s.CHARACTER_Y)) == 2
