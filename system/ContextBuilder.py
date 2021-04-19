1b3d1c04b7670a3eea64916fc214295cc7bd16f9
simon-benigeri <simon.benigeri@protonmail.ch>
2021-04-19 17:44:02 -0500

    some initial code for question answering using roberta from deepset

--- system/ContextBuilder.py
+++ system/ContextBuilder.py
@@ -1,12 +1,49 @@
+from typing import List
 from system.NarrativeFrameCollection import NarrativeFrameCollection
 from system.Script import Script
+
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+import torch
 """
 Component to condition dialogue generation
 """
 class ContextBuilder:
     def __init__(self):
         # ...
-        pass
+        #TODO: make out own model
+        model_name = "deepset/roberta-base-squad2"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+
+    def answer_question(self, question: str, context: str) -> str:
+        """
+        Answers 1 question given the context and the question
+        Args:
+            question:
+            context: text containing the answer
+
+        Returns: answer from the text
+        """
+        #TODO: do we add special tokens?
+        inputs = self.tokenizer(question, context, add_special_tokens=True, return_tensors="pt")
+        input_ids = inputs["input_ids"].tolist()[0]
+
+        # get answers
+        outputs = self.model(**inputs)
+
+        # Get the most likely beginning and end of answer with the argmax of the scores logits
+        answer_start_scores = outputs.start_logits
+        answer_end_scores = outputs.end_logits
+
+        answer_start = torch.argmax(answer_start_scores)
+        answer_end = torch.argmax(answer_end_scores) + 1
+
+        answer = self.tokenizer.convert_tokens_to_string(
+            self.tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end])
+        )
+
+        return answer
+
     
     def generate_context(self, script:Script, narrative_frame_collection:NarrativeFrameCollection):
         # Select matching narrative frame
@@ -25,14 +62,46 @@ if __name__=='__main__':
     model_name = "deepset/roberta-base-squad2"
 
     # a) Get predictions
-    nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    # nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    """
+    
     QA_input = {
         'question': 'Why is model conversion important?',
         'context': 'The option to convert models between FARM and transformers gives freedom to the user and let people easily switch between frameworks.'
     }
     res = nlp(QA_input)
     print(res)
+    """
 
     # b) Load model & tokenizer
     # model = AutoModelForQuestionAnswering.from_pretrained(model_name)
     # tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    # tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    # model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    text = r"""
+    🤗 Transformers (formerly known as pytorch-transformers and pytorch-pretrained-bert) provides general-purpose
+    architectures (BERT, GPT-2, RoBERTa, XLM, DistilBert, XLNet…) for Natural Language Understanding (NLU) and Natural
+    Language Generation (NLG) with over 32+ pretrained models in 100+ languages and deep interoperability between
+    TensorFlow 2.0 and PyTorch.
+    """
+    questions = [
+        "How many pretrained models are available in 🤗 Transformers?",
+        "What does 🤗 Transformers provide?",
+        "🤗 Transformers provides interoperability between which frameworks?",
+    ]
+    for question in questions:
+        inputs = tokenizer(question, text, add_special_tokens=True, return_tensors="pt")
+        input_ids = inputs["input_ids"].tolist()[0]
+        outputs = model(**inputs)
+        answer_start_scores = outputs.start_logits
+        answer_end_scores = outputs.end_logits
+        answer_start = torch.argmax(
+            answer_start_scores
+        )  # Get the most likely beginning of answer with the argmax of the score
+        answer_end = torch.argmax(
+            answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+        print(f"Question: {question}")
+        print(f"Answer: {answer}")
