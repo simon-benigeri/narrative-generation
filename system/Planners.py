0c68ae199b097fe8fcf0800b51a599876a1cc5f6
simon-benigeri <simon.benigeri@protonmail.ch>
2021-04-26 17:58:49 -0500

    deleted useless ROCStories datasets. we only use the full one for now

--- system/Planners.py
+++ system/Planners.py
@@ -2,7 +2,5 @@ import enum
 
 class Planners(enum.Enum):
     DEFAULT = 0
-    ROCStories__spring2016 = 1
-    ROCStories__winter2017 = 2
-    ROCStories__full = 3
-    GLUCOSE = 4
+    ROCStories = 1
+    GLUCOSE = 2
