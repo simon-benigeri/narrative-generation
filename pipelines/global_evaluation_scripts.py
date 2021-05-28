5fde74893bb9ef629eab35c70b2a461b18473390
simon-benigeri <simon.benigeri@protonmail.ch>
2021-05-28 10:49:11 -0500

    add global eval

---
+++ pipelines/global_evaluation_scripts.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.metrics import classification_report
+
+# Config
+GPT_MODEL = os.environ.get('GPT_MODEL', 'gpt2')
+EPOCHS = int(os.environ.get('EPOCHS', 10))
+EMOTIONS = ['joy', 'love', 'fear', 'sadness', 'anger', 'surprise']
+
+CSV_RESULTS_DIR = 'results/emotion_labels.csv'
+PATHS = [f'../models/{GPT_MODEL}/{emotion}/{EPOCHS}_epochs/{CSV_RESULTS_DIR}' for emotion in EMOTIONS]
+
+SAVE_RESULTS_DIR = os.environ.get('SAVE_RESULTS_DIR', f'models/{GPT_MODEL}/results')
+
+def evaluate(filepaths):
+    
+    df = pd.concat(map(pd.read_csv, filepaths))
+    df = df.astype({'target_labels': 'str',
+                    'target_confidence': 'float',
+                    'predicted_labels': 'str',
+                    'predicted_confidence': 'float'
+                    })
+    df = df.dropna()
+
+    y_true = df['target_labels'].to_numpy()
+    y_pred = df['predicted_labels'].to_numpy()
+    
+    report = classification_report(y_true, y_pred, labels=EMOTIONS, target_names=EMOTIONS)
+    mean_averages = df.groupby('target_labels')['target_confidence'].mean()
+    
+    return report, mean_averages, y_pred
+
+if __name__=='__main__':
+	
+	print("Evaluating...")
+	report, mean_averages = evaluate(filepaths=PATHS)
+	formatted_results = f"RESULTS\n{report}"
+
+	if not os.path.exists(SAVE_RESULTS_DIR):
+        os.makedirs(SAVE_RESULTS_DIR)
+        
+    with open(SAVE_RESULTS_DIR + "/results.txt", "w") as f:
+        f.write(formatted_results)
+
+    print("Done.")
+	
